0482552566c468d3724f1b9936b6d7978dae13c9
Gabriel-Onado <gabriel.onado@edu.esiee.fr>
2025-11-06 10:04:48 +0000

    fonction nombre premier

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
+"""fonction de math"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    return if p is a prime number or not (boolean)
 
-    # votre code ici
+    Args:
+        p: positive integer
 
-    pass
+    Returns:
+        isprime(p) : p = i*a : false
+
+        or 
+
+        isprime(p) : p : true
+
+    >>>isprime(5)
+    5 : true    
+    >>>isprime(10)
+    10 = 2 * 5 : false
+
+
+    """
+    ispprime = True
+    if p == 1:
+        print("1 : false")
+        return False
+    if p == 0:
+        print("0 : false")
+        return False
+
+    for i in range(2,  round(sqrt(p))+1):
+        if p % i == 0 :
+            ispprime = False
+            print(f"{p} = {i} * {p//i} : false")
+            return False
+    if ispprime:
+        print(f"{p} : true")
+        return True
+    return ispprime
 
 #### Fonction principale
 
 
 def main():
+    """
+    main fonction
+    """
 
     # vos appels à la fonction secondaire ici
 
@@ -20,7 +57,6 @@ def main():
         if isprime(n):
             print(n, end=", ")
 
-    print()
 
 
 if __name__ == "__main__":
